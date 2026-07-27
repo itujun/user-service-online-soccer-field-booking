e3f889906e9459f20f38d2e4e9b255d6b6caef7d
juna <itujun7@gmail.com>
2026-07-28 01:31:16 +0700

    cmd/main.go bagian 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,29 @@
 package cmd
 
 import (
+	"fmt"
+	"net/http"
 	"time"
+	"user-service/common/response"
 	"user-service/config"
+	"user-service/constants"
+	"user-service/controllers"
 	"user-service/database/seeders"
 	"user-service/domain/models"
+	"user-service/middlewares"
 	"user-service/repositories"
+	"user-service/routes"
+	"user-service/services"
 
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
 
 var command = &cobra.Command{
-	Use: "serve",
+	Use:   "serve",
 	Short: "Start the server",
 	Run: func(c *cobra.Command, args []string) {
 		_ = godotenv.Load()
@@ -41,5 +52,53 @@ var command = &cobra.Command{
 		repository := repositories.NewRepositoryRegistry(db)
 		service := services.NewServiceRegistry(repository)
 		controller := controllers.NewControllerRegistry(service)
+
+		router := gin.Default()
+		// panggil handle panic
+		router.Use(middlewares.HandlePanic())
+		// handle route not found
+		router.NoRoute(func(c *gin.Context) {
+			c.JSON(http.StatusNotFound, response.Response{
+				Status:  constants.Error,
+				Message: fmt.Sprintf("Path %s", http.StatusText(http.StatusNotFound)),
+			})
+		})
+		// router default
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, response.Response{
+				Status:  constants.Success,
+				Message: "Welcome to User Service",
+			})
+		})
+		// handle CORS
+		router.Use(func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT")
+			// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Next()
+		})
+
+		// rate limiter
+		lmt := tollbooth.NewLimiter(
+			float64(config.Config.RateLimiterMaxRequest),
+			&limiter.ExpirableOptions{
+				DefaultExpirationTTL: time.Duration(config.Config.RateLimiterTimeSecond) * time.Second,
+			})
+		router.Use(middlewares.RateLimiter(lmt))
+
+		group := router.Group("/api/v1")
+		route := routes.NewRouteRegistry(controller, group)
+		route.Serve()
+
+		port := fmt.Sprintf(":%d", config.Config.Port)
+		router.Run(port)
+	},
+}
+
+func Run() {
+	err := command.Execute()
+	if err != nil {
+		panic(err)
 	}
 }
