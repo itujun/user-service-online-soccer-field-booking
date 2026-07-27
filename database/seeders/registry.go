8b82d29f286e6bf38cb9e4c47802a2f2ea0f5b32
juna <itujun7@gmail.com>
2026-07-27 21:57:00 +0700

    Setup Seeder

---
+++ database/seeders/registry.go
@@ -0,0 +1,22 @@
+package seeders
+
+import "gorm.io/gorm"
+
+type Registry struct {
+	db *gorm.DB
+}
+
+type ISeederRegistry interface {
+	Run()
+}
+
+func NewRegistry(db *gorm.DB) ISeederRegistry {
+	return &Registry{
+		db: db,
+	}
+}
+
+func (s *Registry) Run() {
+	RunRoleSeeder(s.db)
+	RunUserSeeder(s.db)
+}
